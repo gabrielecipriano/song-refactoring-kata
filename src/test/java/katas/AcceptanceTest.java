e2b797ad74ac4723982ab4722cb4beab346b1865
Gabriele Cipriano <gabri.cipriano@gmail.com>
2019-02-07 15:50:12 +0000

    initial commmit

---
+++ src/test/java/katas/AcceptanceTest.java
@@ -0,0 +1,13 @@
+package katas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class AcceptanceTest {
+    @Test
+    void should_pass() {
+        assertThat(true, is(true));
+    }
+}
