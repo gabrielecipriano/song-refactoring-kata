aee805d437641b76d7ae0d4af254b6d1de1b0a6b
nawhi <25394965+nawhi@users.noreply.github.com>
2019-02-15 17:00:35 +0000

    Use List<Animal> instead of Animal[]

--- src/main/java/katas/Song.java
+++ src/main/java/katas/Song.java
@@ -1,10 +1,11 @@
 package katas;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import static java.lang.String.*;
+import static java.util.Arrays.asList;
 
 public class Song {
     public static void main(String[] args) {
@@ -13,7 +14,7 @@ public class Song {
 
     public void execute() {
 
-        String song = composeSongWith(new Animal[] {
+        final List<Animal> animals = asList(
                 new Animal("fly", "I don't know why she swallowed a fly - perhaps she'll die!"),
                 new Animal("spider", "That wriggled and wiggled and tickled inside her."),
                 new Animal("bird", "How absurd to swallow a bird."),
@@ -21,39 +22,40 @@ public class Song {
                 new Animal("dog", "What a hog, to swallow a dog!"),
                 new Animal("cow", "I don't know how she swallowed a cow!"),
                 new Animal("horse", "...She's dead, of course!")
-        });
+        );
+        String song = composeSongWith(animals);
 
         System.out.print(song);
     }
 
-    private String composeSongWith(Animal[] animals) {
-        return exposition(animals[0], ".") +
+    private String composeSongWith(List<Animal> animals) {
+        return exposition(animals.get(0), ".") +
                 "\n" +
                 verses(animals) +
                 "\n" +
-                exposition(animals[animals.length - 1], "...");
+                exposition(animals.get(animals.size() - 1), "...");
     }
 
-    private String verses(Animal[] animals) {
-        return IntStream.range(2, animals.length)
-                .mapToObj(i -> verse(Arrays.copyOfRange(animals, 0, i)))
+    private String verses(List<Animal> animals) {
+        return IntStream.range(2, animals.size())
+                .mapToObj(i -> verse(animals.subList(0, i)))
                 .collect(Collectors.joining("\n"));
     }
 
-    private String verse(Animal... animals) {
-        Animal last = animals[animals.length - 1];
-        Animal firstAnimal = animals[0];
+    private String verse(List<Animal> animals) {
+        Animal last = animals.get(animals.size() - 1);
+        Animal firstAnimal = animals.get(0);
 
         return exposition(last, ";") +
                 development(animals) +
                 coda(firstAnimal);
     }
 
-    private String development(Animal... animals) {
+    private String development(List<Animal> animals) {
         StringBuilder sb = new StringBuilder();
-        for (int i = animals.length - 1; i > 0; i--) {
+        for (int i = animals.size() - 1; i > 0; i--) {
             final String punctuation = i == 1 ? ";" : ",";
-            sb.append(format("She swallowed the %s to catch the %s%s\n", animals[i].name, animals[i - 1].name, punctuation));
+            sb.append(format("She swallowed the %s to catch the %s%s\n", animals.get(i).name, animals.get(i - 1).name, punctuation));
         }
         return sb.toString();
     }
